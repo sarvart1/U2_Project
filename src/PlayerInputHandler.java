a58c6baa03f8e6d6dcf2680178e6c27e04a9adaa
SarvarTuychiev <sarvart@nycstudents.net>
2025-11-17 14:40:29 -0500

    Initial commit

---
+++ src/PlayerInputHandler.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class PlayerInputHandler {
+    private Scanner scanner = new Scanner(System.in);
+    private final int WORD_LENGTH = 7;
+    
+
+}
---
+++ src/WordleGame.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+import java.util.List;
+
+public class WordleGame {
+    private String secretCountry;
+    private int attemptsLeft = 6;
+
+
+}
