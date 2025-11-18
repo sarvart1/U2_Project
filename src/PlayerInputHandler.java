4e0ed8f0a41ef350ac1bbc92738d8ee508000fce
sarvart1 <sarvart@nycstudents.net>
2025-11-18 14:48:09 -0500

    Refactor WORD_LENGTH to wordLength and update usage

--- src/PlayerInputHandler.java
+++ src/PlayerInputHandler.java
@@ -1,7 +1,25 @@
 import java.util.Scanner;
 public class PlayerInputHandler {
     private Scanner scanner = new Scanner(System.in);
-    private final int WORD_LENGTH = 7;
-    
+    private final int wordLength = 7;
 
+    public String getPlayerGuess() {
+        String guess;
+        while (true) {
+            System.out.print("Enter your " + wordLength + "-letter country guess: ");
+            guess = scanner.nextLine().toUpperCase();
+            if (guess.length() == wordLength) {
+                break;
+            } else {
+                System.out.print("Invalid guess. Enter a " + wordLength + "-letter country: ");
+            }
+        }
+        return guess;
+
+
+    }
+
+    public void closeScanner() {
+        scanner.close();
+    }
 }
