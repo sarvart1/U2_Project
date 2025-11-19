780523a0909449ff5787d2308f45429a9c63920f
sarvart1 <sarvart@nycstudents.net>
2025-11-19 14:45:16 -0500

    Add word loading and random selection functionality
    Implemented methods to load words from a file and select a random word.

--- src/WordLoader.java
+++ src/WordLoader.java
@@ -3,7 +3,31 @@ import java.util.Random;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class WordLoader {
+    private final List<String> wordList = new ArrayList<>();
+    private final String fileName = "countries.txt";
 
+    public WordLoader () throws FileNotFoundException {
+        loadWords();
+
+
+    }
+    private void loadWords() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(fileName));
+        while(scanner.hasNextLine()) {
+            String word = scanner.nextLine().toUpperCase();
+            if(!word.isEmpty()) {
+                wordList.add(word);
+        }
+        }
+        scanner.close();
+
+    }
+    public String selectRandomWord () {
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(wordList.size());
+        return wordList.get(randomIndex);
+    }
 }
