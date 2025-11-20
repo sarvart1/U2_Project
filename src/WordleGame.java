68900aca53784447afebe91b2bf45ac04c5cd0a3
sarvart1 <sarvart@nycstudents.net>
2025-11-20 14:45:03 -0500

    working on playGame method right now. set up ints and strings
    Refactor WordleGame to use a final secretWord and adjust attempts.

--- src/WordleGame.java
+++ src/WordleGame.java
@@ -2,8 +2,26 @@ import java.util.Random;
 import java.util.List;
 
 public class WordleGame {
-    private String secretCountry;
+    private final String secretWord;
     private int attemptsLeft = 6;
+    private final PlayerInputHandler inputHandler;
+    private final int maxGuesses = 6;
+    private final int wordLength = 7;
+
+    private int guessesRemaining;
+
+    public WordleGame(String secretWord, PlayerInputHandler inputHandler) {
+        this.secretWord = secretWord;
+        this.inputHandler = inputHandler;
+        this.guessesRemaining = maxGuesses;
+    }
+    public void playGame() {
+        System.out.println("Welcome to Countries Wordle! Guess the " + wordLength + "-letter country.");
+        
+
+
+    }
+
 
 
 }
