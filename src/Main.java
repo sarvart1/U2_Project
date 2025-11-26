60395f80278aa8f5e47a4caed92be664b9cb2d37
sarvart1 <sarvart@nycstudents.net>
2025-11-26 14:38:36 -0500

    Modify main method to include exception handling
    Updated main method to handle exceptions and added game initialization.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IllegalArgumentException {
+        WordLoader loader = new WordLoader();
+        String secretCountry = loader.selectRandomWord();
+        PlayerInputHandler inputHandler = new PlayerInputHandler();
+        WordleGame game = new WordleGame(secretCountry, inputHandler);
+        game.playGame();
 
 
     }
